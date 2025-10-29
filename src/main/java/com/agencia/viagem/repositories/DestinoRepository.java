62a10411b8d6378f0db7f4edb367f9703b946c1c
Maria Aline Mees <mariaalinemees@gmail.com>
2025-10-28 21:15:05 -0300

    Clarify
    Updated method comment to clarify its functionality.

--- src/main/java/com/agencia/viagem/repositories/DestinoRepository.java
+++ src/main/java/com/agencia/viagem/repositories/DestinoRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface DestinoRepository extends JpaRepository<Destino, Long> {
-    // Busca destinos cujo nome contenha a string fornecida, ignorando maiúsculas/minúsculas.
+   
     List<Destino> findByNomeContainingIgnoreCase(String nome);
 }
